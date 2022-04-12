3213dae8157c684d31d4d5a94274204ea340dea6
Jawn Vawka <jawn.vawka@gmail.com>
2022-04-12 15:11:19 -0500

    Add userPlay()

--- script.js
+++ script.js
@@ -9,4 +9,17 @@ function computerPlay() {
     }
 }
 
-console.log(computerPlay())
+function userPlay() {
+    checkChoice = true;
+    while (checkChoice) {
+        userChoice = prompt('Choose either Rock, Paper, or Scissors');
+        userChoice = userChoice.slice(0, 1).toUpperCase() + userChoice.slice(1).toLowerCase();
+        if (userChoice == 'Rock' || userChoice == 'Paper' || userChoice == 'Scissors') {
+            return userChoice;
+        } else {
+            alert('You did not enter a valid choice. Please try again.');
+        }
+    }
+}
+
+console.log(userPlay());
