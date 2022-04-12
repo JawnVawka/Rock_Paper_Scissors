6266c8c7572d35b195182961c881331fd5a4bc20
Jawn Vawka <jawn.vawka@gmail.com>
2022-04-12 18:36:51 -0500

    Add calcPoints() and game()

--- script.js
+++ script.js
@@ -29,15 +29,45 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection == 'Rock' && computerSelection == 'Scissors' ||
         playerSelection == 'Paper' && computerSelection == 'Rock' ||
         playerSelection == 'Scissors' && computerSelection == 'Paper') {
+            console.log();
             return `You Win! ${playerSelection} beats ${computerSelection}`;
-      } else if (playerSelection == computerSelection) {
+    } else if (playerSelection == computerSelection) {
+        console.log();
         return `It's a tie! You both chose ${computerSelection}`;
-      } else {
-          return `You Lose! ${computerSelection} beats ${playerSelection}`;
-      }
+    } else {
+        console.log();
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    }
+}
+// calculate points and declare winner
+function calcPoints(playerPoints, computerPoints) {
+    if (playerPoints > computerPoints) {
+        return `You Win! You: ${playerPoints} Computer: ${computerPoints}`;
+    } else if (playerPoints < computerPoints) {
+        return `You Lose! You: ${playerPoints} Computer: ${computerPoints}`;
+    } else {
+        return `You Tied! You: ${playerPoints} Computer: ${computerPoints}`;
+    }
+}
+// play 5 rounds of rock, paper, scissors
+function game() {
+    let playerPoints = 0;
+    let computerPoints = 0;
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = userPlay();
+        const computerSelection = computerPlay();
+        let result = playRound(playerSelection, computerSelection);
+
+        console.log(result);
+
+        if (result.slice(0, result.indexOf('!')) == 'You Win') {
+            playerPoints += 1;
+        } else if (result.slice(0, result.indexOf('!')) == 'You Lose') {
+            computerPoints += 1;
+        }
+    }
+
+    console.log(calcPoints(playerPoints, computerPoints));
 }
 
-const playerSelection = userPlay();
-const computerSelection = computerPlay();
-
-console.log(playRound(playerSelection, computerSelection));
+console.log(game());
