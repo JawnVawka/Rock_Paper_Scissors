0b77bcccd1bfe127eceaefdd10355fde33836f14
Jawn Vawka <jawn.vawka@gmail.com>
2022-04-12 14:26:26 -0500

    Create computerPlay()

--- script.js
+++ script.js
@@ -1,58 +1,12 @@
 function computerPlay() {
-    const choices = ['Rock', 'Paper', 'Scissors'];
-    const random = Math.floor(Math.random() * choices.length);
-    return choices[random];
-}
-
-// Ask user for their choice
-function userChoice() {
-    return prompt('Choose Rock, Paper, or Scissors');
-}
-
-// Ask user for their choice
-function errorChoice() {
-    return prompt('You entered an invalid choice. Choose Rock, Paper, or Scissors');
-}
-
-// Capitalize user choice
-function capitalize(word) {
-    return word[0].toUpperCase() + word.slice(1).toLowerCase();
-}
-
-// Check to make sure they entered a valid choice
-function validateChoice() {
-    let choice = capitalize(userChoice());
-    let repeat = true;
-    while (repeat) {
-        if (choice == 'Rock' || choice == 'Paper' || choice == 'Scissors') {
-            repeat = false;
-            return choice;
-        } else {
-            choice = capitalize(errorChoice());
-        }
-    }[ ]
-
-// create game to check who won
-function playRound (playerSeleciton, computerSelection) {
-    if ((playerSeleciton == 'Rock' && computerSelection == 'Scissors') || (playerSeleciton == 'Paper' && computerSelection == 'Rock') || (playerSeleciton == 'Scissors' && computerSelection == 'Paper')) {
-        return `You Win! ${playerSeleciton} beats ${computerSelection}`;
-    } else if ((playerSeleciton == 'Rock' && computerSelection == 'Paper') || (playerSeleciton == 'Paper' && computerSelection == 'Scissors') || (playerSeleciton == 'Scissors' && computerSelection == 'Rock')) {
-        return `You Lose! ${computerSelection} beats ${playerSeleciton}`;
+    const randNum = Math.floor(Math.random() * 3 + 1);
+    if (randNum == 1) {
+        return 'Rock';
+    } else if (randNum == 2) {
+        return 'Paper';
     } else {
-        return `It's a Tie! You both chose ${playerSeleciton}`;
+        return 'Scissors'
     }
 }
 
-function game() {
-    //repeat playRound() 5 times
-    let count = 0;
-    for (let i = 0; i < 5; i++) {
-        validateChoice();
-    }
-    return count;
-}
-
-// let playerSeleciton = validateChoice();
-let computerSelection = computerPlay();
-
-console.log(game());
+console.log(computerPlay())
