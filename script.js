418bb06c6db0b4e7c82584fa781d3fa1b270b35c
Jawn Vawka <jawn.vawka@gmail.com>
2022-04-28 22:39:54 -0500

    Rewrite script to work with UI elements

--- script.js
+++ script.js
@@ -10,64 +10,63 @@ function computerPlay() {
     }
 }
 
-// ask user for input (Rock, Paper, or Scissors)
-function userPlay() {
-    checkChoice = true;
-    while (checkChoice) {
-        userChoice = prompt('Choose either Rock, Paper, or Scissors');
-        userChoice = userChoice.slice(0, 1).toUpperCase() + userChoice.slice(1).toLowerCase();
-        if (userChoice == 'Rock' || userChoice == 'Paper' || userChoice == 'Scissors') {
-            return userChoice;
-        } else {
-            alert('You did not enter a valid choice. Please try again.');
+function checkWinner() {
+    if (playerChoice === 'Rock' && computerChoice === 'Scissors' ||
+        playerChoice === 'Paper' && computerChoice === 'Rock' ||
+        playerChoice === 'Scissors' && computerChoice === 'Paper') {
+        playerPoints += 1;
+    } else if (computerChoice === playerChoice) {
+        return;
+    } else {
+        computerPoints += 1;
+    };
+}
+
+function removeImages() {
+    const parent = document.getElementById('playerOptions');
+    const child = document.getElementsByTagName('img');
+    if (playerPoints == 5 || computerPoints == 5) {
+        for (let i = 0; i < child.length; i) {
+            parent.removeChild(child[i]);
         }
     }
 }
 
-// Play a round of Rock, Paper, Scissors
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection == 'Rock' && computerSelection == 'Scissors' ||
-        playerSelection == 'Paper' && computerSelection == 'Rock' ||
-        playerSelection == 'Scissors' && computerSelection == 'Paper') {
-            console.log();
-            return `You Win! ${playerSelection} beats ${computerSelection}`;
-    } else if (playerSelection == computerSelection) {
-        console.log();
-        return `It's a tie! You both chose ${computerSelection}`;
-    } else {
-        console.log();
-        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+function declareWinner() {
+    removeImages();
+    const parent = document.getElementById('playerOptions');
+    const child = document.createElement('img')
+
+    if (playerPoints == 5) {
+        child.src = 'images/winner.svg';
+        parent.appendChild(child);
+    } else if (computerPoints == 5) {
+        child.src = 'images/loser.svg';
+        parent.appendChild(child);
     }
 }
-// calculate points and declare winner
-function calcPoints(playerPoints, computerPoints) {
-    if (playerPoints > computerPoints) {
-        return `You Win! You: ${playerPoints} Computer: ${computerPoints}`;
-    } else if (playerPoints < computerPoints) {
-        return `You Lose! You: ${playerPoints} Computer: ${computerPoints}`;
-    } else {
-        return `You Tied! You: ${playerPoints} Computer: ${computerPoints}`;
-    }
-}
-// play 5 rounds of rock, paper, scissors
-function game() {
-    let playerPoints = 0;
-    let computerPoints = 0;
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = userPlay();
-        const computerSelection = computerPlay();
-        let result = playRound(playerSelection, computerSelection);
 
-        console.log(result);
+function playRound(choice) {
+    computerChoice = computerPlay();
+    playerChoice = choice.target.className;
 
-        if (result.slice(0, result.indexOf('!')) == 'You Win') {
-            playerPoints += 1;
-        } else if (result.slice(0, result.indexOf('!')) == 'You Lose') {
-            computerPoints += 1;
-        }
-    }
+    checkWinner();
 
-    console.log(calcPoints(playerPoints, computerPoints));
+    playerPointDisplay.textContent = `Player: ${playerPoints}`;
+    computerPointDisplay.textContent = `Computer: ${computerPoints}`;
+
+    declareWinner();
 }
 
-console.log(game());
+let playerPoints = 0;
+let computerPoints = 0;
+
+const choiceId = document.getElementById('playerOptions');
+const choiceArray = choiceId.getElementsByTagName('img');
+
+let playerPointDisplay = document.querySelector(".userScore");
+let computerPointDisplay = document.querySelector(".computerScore");
+
+for (i = 0; i < choiceArray.length; i++) {
+    choiceArray[i].addEventListener("click", playRound);
+}
