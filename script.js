92dea8a990e762a0f5e42ee5f91711d6db20aa27
Jawn Vawka <jawn.vawka@gmail.com>
2022-04-30 18:47:06 -0500

    Add "Play Again" button and add round results display

--- script.js
+++ script.js
@@ -15,10 +15,12 @@ function checkWinner() {
         playerChoice === 'Paper' && computerChoice === 'Rock' ||
         playerChoice === 'Scissors' && computerChoice === 'Paper') {
         playerPoints += 1;
+        resultOutput('You have won!');
     } else if (computerChoice === playerChoice) {
-        return;
+        resultOutput('You have tied!');
     } else {
         computerPoints += 1;
+        resultOutput('You have lost!');
     };
 }
 
@@ -36,25 +38,49 @@ function declareWinner() {
     removeImages();
     const parent = document.getElementById('playerOptions');
     const child = document.createElement('img')
+    const result = document.getElementById('gameResult');
 
     if (playerPoints == 5) {
         child.src = 'images/winner.svg';
         parent.appendChild(child);
+        document.body.removeChild(result);
+        playAgain();
     } else if (computerPoints == 5) {
         child.src = 'images/loser.svg';
         parent.appendChild(child);
+        document.body.removeChild(result);
+        playAgain();
     }
+}
+
+function resultOutput(result) {
+    if(document.getElementById('gameResult')) {
+        const divResult =  document.getElementById('gameResult');
+        divResult.textContent = `${result}`;
+    } else {
+        const parentDiv = document.getElementById('playerOptions');
+        const newDiv = document.createElement('div');
+        newDiv.setAttribute('id', 'gameResult');
+        document.body.appendChild(newDiv);
+        newDiv.textContent = `${result}`;
+    }
+}
+
+function playAgain() {
+    const parentDiv = document.createElement('div');
+    const newButton = document.createElement('button');
+    parentDiv.setAttribute('id', 'playAgain');
+    newButton.textContent = `Play Again`;
+    document.body.appendChild(parentDiv);
+    parentDiv.appendChild(newButton).addEventListener("click", () => location.reload());
 }
 
 function playRound(choice) {
     computerChoice = computerPlay();
     playerChoice = choice.target.className;
-
     checkWinner();
-
     playerPointDisplay.textContent = `Player: ${playerPoints}`;
     computerPointDisplay.textContent = `Computer: ${computerPoints}`;
-
     declareWinner();
 }
 
